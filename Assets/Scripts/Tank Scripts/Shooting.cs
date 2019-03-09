4595b6dedc13ee288ec306bd958b8422cdbca4d8
jewelsz <jowelle_klomp@hotmail.com>
2019-03-09 22:15:10 +0100

    Initial commit

---
+++ Assets/Scripts/Tank Scripts/Shooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bullet;
+    [SerializeField]
+    Transform bulletSpawn;
+
+    float nextFire;
+    float fireRate = 1;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Fire();
+        }
+    }
+
+    private void Fire()
+    {
+
+        bullet = Instantiate(
+            bullet,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        //bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+       
+    }
+}
